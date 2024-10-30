aa4da8d96dd369fe2d8addea1c3e38a0c53cb953
nihindug2004 <nihindug2004@gmail.com>
2024-10-30 11:02:13 +0530

    add css

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-console.log(document.getElementById("heading").innerHTML="Guess The Number...?");
-
-
-let randomNumber = Math.floor((Math.random()*10)+1);
-console.log(randomNumber);
+function guessFunction(){
+    let randomNumber = Math.floor((Math.random()*10)+1);
+    //console.log(randomNumber);
+    
+    const num1 = parseFloat(document.getElementById('num').value);
+    let result;
+    if(num1==randomNumber){
+        result="You win!";
+        alert(result);
+    } else if(num1>randomNumber){
+        result="Too high! Try again.";
+        alert(result);
+    }else if(num1<randomNumber){
+        result="Too low! Try again.";
+        alert(result);
+    }else if(num1!=randomNumber){
+        result="Invalid input";
+        alert(result);
+    }
+    
+    
+    document.getElementById('output').innerHTML= result;
+    }
